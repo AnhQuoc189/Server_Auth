2d38de00294edb5d15d2c08fdd65cb56739facf9
AnhQuoc189 <binbin18092003@gmail.com>
2023-04-25 19:29:38 +0700

    second commit

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDb = async () => {
     try {
-        const connect = await mongoose.connect(process.env.CONNECTION_STRING, {
+        const connect = await mongoose.connect('mongodb+srv://quocanh:quocanh-1809@cluster0.jo6e3wp.mongodb.net/APP_AUTH', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
